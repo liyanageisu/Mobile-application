7ad9838d987b4e1bdab0eb412f72d20e6b1958fd
liyanageisu <isu110996@gmail.com>
2020-05-08 14:07:23 +0530

    1st com

---
+++ project/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    new Container(
+      decoration: new BoxDecoration(color: Colors.blue),
+      child:new Center(
+       child:new Text("Hello world")
+             )
+
+        )
+    );
+   
+}
